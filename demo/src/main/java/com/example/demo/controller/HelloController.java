c1827afb865b7596f1b4259e5a7b943a397901f4
silvergold799 <silvergold799@biztechi.com>
2023-01-17 17:55:13 +0900

    222

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -3,8 +3,6 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,18 +13,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class HelloController {
-	private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
 	
 	@RequestMapping("/")
 	public ModelAndView main() {
-		log.info("main"); // default
 		return new ModelAndView("/index");
 	}
 	
 	@RequestMapping("/index")
 	public ModelAndView index(ModelAndView mav) {
-		
-		log.info("index"); // default 
 		return mav;
 	}
 	
